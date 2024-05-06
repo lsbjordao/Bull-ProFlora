60436c96a3e2063c4e2ef46772c87da574005738
lsbjordao <tucarj@gmail.com>
2024-05-06 13:24:11 -0300

    cnc

--- src/queues/helpers/getOccFromProFlora.ts
+++ src/queues/helpers/getOccFromProFlora.ts
@@ -4,26 +4,40 @@ import * as dotenv from 'dotenv';
 dotenv.config()
 
 async function sendGetRequest(taxon: string) {
+
     try {
-        console.log(taxon)
-        const taxonId = '1660'
-        const url = `${process.env.ProFloraDev}/occurrence?taxonId=${taxonId}`;
         const config: any = {
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
                 'Authorization': `Bearer ${process.env.ProFloraToken}`
             }
-        };
-        const response = await axios.get(url, config)
+        }
+
+        let urlBase = process.env.ProFloraProd
+        if (process.env.NODE_ENV === 'dev') { urlBase = process.env.ProFloraDev }
+        
+        const endpoint_getTaxonId = `${urlBase}/get-taxon-id-by-scientificname?scientificname=${taxon}`;
+
+        const taxonId = await axios.get(endpoint_getTaxonId, config)
+            .then(response => {
+                return response.data._embedded.get_taxon_id_by_scientificname[0].id
+            })
+            .catch(error => {
+                console.error('Error getting taxon ID:', error);
+            })
+
+        const endpoint_getOccs = `${urlBase}/occurrence?taxonId=${taxonId}`;
+
+        const responseOccs = await axios.get(endpoint_getOccs, config)
             .then(response => {
                 return response.data._embedded.occurrence
             })
             .catch(error => {
-                console.error('Erro:', error);
+                console.error('Error getting occurrences:', error);
             });
 
-        return response
+        return responseOccs
     } catch (error) {
         console.error('Ocorreu um erro ao enviar a requisição:', error.message);
     }
@@ -31,14 +45,14 @@ async function sendGetRequest(taxon: string) {
 
 async function getOccFromProFlora(taxon: string) {
     const data: any = await sendGetRequest(taxon)
-    
+
     const result: any = {
         n: data.length,
         ids: data.map((occ: any) => occ.id),
         validationRecords: data.map((occ: any) => occ.isValid),
         validationSIG: data.map((occ: any) => occ.gisStatusId),
         coordsObj: data.map((occ: any) => {
-            return { 
+            return {
                 lat: occ.latitude,
                 lon: occ.longitude,
                 precision: occ.georeferencePrecisionId,
