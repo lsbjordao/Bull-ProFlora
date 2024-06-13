aeba49ed6525295bc0d1f7d76bfa9171a0ef6304
lsbjordao <tucarj@gmail.com>
2024-06-13 09:32:10 -0300

    cnc

--- src/queues/records.processor.ts
+++ src/queues/records.processor.ts
@@ -8,7 +8,7 @@ import { Logger } from '@nestjs/common';
 import { Job } from 'bullmq';
 
 import { getOcc } from './helpers/getOccFromOldSys';
-import { getOccFromProFlora } from './helpers/getOccFromProFlora'
+import { getOccFromProFlora } from './helpers/getOccFromProFlora';
 import { whichFlow } from './helpers/whichFlow';
 import { GeoJSON } from './helpers/geojson';
 import { getSIGanalyst } from './helpers/getSIGanalyst';
@@ -51,228 +51,74 @@ export class Processor_records extends WorkerHost {
       speciesCoords = speciesOcc.coordsObj;
     }
 
-    if (
-      source === 'Museu-Goeldi/PA' ||
-      source === 'CNCFlora-ProFlora'
-    ) {
+    if (source === 'Museu-Goeldi/PA' || source === 'CNCFlora-ProFlora') {
       speciesOcc = await getOccFromProFlora(species);
-      if (Array.isArray(speciesOcc.validationRecords) &&
-        speciesOcc.validationRecords.every((record: any) => record === null)) {
-          job.log(`All validation records null.`);
+      if (
+        Array.isArray(speciesOcc.validationRecords) &&
+        speciesOcc.validationRecords.every(
+          (record: any) => record === null
+        )
+      ) {
+        job.log(`All validation records null.`);
         throw new Error('All validation records null.');
       }
       speciesIds = speciesOcc.occIds;
 
       const occIsValid: any = {
-        "true": "Válido",
-        "false": "Inválido"
-      }
+        true: 'Válido',
+        false: 'Inválido'
+      };
       speciesValidationOcc = speciesOcc.validationRecords;
-      speciesValidationOcc = speciesOcc.validationRecords.map((value: any) => occIsValid[value])
+      speciesValidationOcc = speciesOcc.validationRecords.map((value: any) =>
+        occIsValid[value]
+      );
 
       const occStatusGis: any = {
-        "0": "SIG NOT OK",
-        "1": "SIG OK",
-        "2": "SIG NOT OK"
-      }
-      speciesValidationSIG = speciesOcc.validationSIG.map((value: any) => occStatusGis[value])
+        '0': 'SIG NOT OK',
+        '1': 'SIG OK',
+        '2': 'SIG NOT OK'
+      };
+      speciesValidationSIG = speciesOcc.validationSIG.map((value: any) =>
+        occStatusGis[value]
+      );
 
       const occGeoreferenceProtocol: any = {
-        "1": "Google Earth",
-        "2": "SIG",
-        "3": "Coletor"
-      }
+        '1': 'Google Earth',
+        '2': 'SIG',
+        '3': 'Coletor'
+      };
       const occGeoprecision: any = {
-        "1": "0 a 250m",
-        "2": "250 a 1000m",
-        "3": "5 a 10km",
-        "4": "50 a 100km",
-        "5": "10 a 20 km",
-        "6": "50 a 100km",
-        "7": "Centróide de UC",
-        "8": "Centróide de município"
-      }
-      speciesCoords = speciesOcc.coordsObj
-      speciesCoords = speciesCoords.map((coord: any) => {
-        return {
-          ...coord,
-          precision: occGeoprecision[coord.precision.toString()],
-          protocol: occGeoreferenceProtocol[coord.protocol.toString()]
-        };
-      });
+        '1': '0 a 250m',
+        '2': '250 a 1000m',
+        '3': '1 a 5km',
+        '4': '5 a 10km',
+        '5': '10 a 20 km',
+        '6': '50 a 100km',
+        '7': 'Centróide de UC',
+        '8': 'Centróide de município',
+        '9': '20 a 50km'
+      };
+      speciesCoords = speciesOcc.coordsObj;
+      const speciesCoordsIds = speciesOcc.occIds;
+
+      speciesCoords = speciesCoords.map((coord: any, idx: number) => ({
+        id: speciesCoordsIds[idx],
+        ...coord,
+        precision: occGeoprecision[coord.precision?.toString()],
+        protocol: occGeoreferenceProtocol[coord.protocol?.toString()]
+      }));
     }
 
-    let flowData: any = ''
-    let SIGanalyst: any = ''
+    let flowData: any = '';
+    let SIGanalyst: any = '';
 
-    if (source === 'Museu-Goeldi/PA') {
-      flowData = await whichFlow(species, 'Museu-Goeldi/PA')
-      SIGanalyst = await getSIGanalyst(species, 'Museu-Goeldi/PA')
-    }
-
-    if (source === 'CNCFlora-ProFlora') {
-      flowData = await whichFlow(species, 'CNCFlora-ProFlora')
-      SIGanalyst = await getSIGanalyst(species, 'CNCFlora-ProFlora')
-    }
-
-    if (source === 'CNCFlora-oldSystem') {
-      flowData = await whichFlow(species, 'CNCFlora-oldSystem')
-      SIGanalyst = await getSIGanalyst(species, 'CNCFlora-oldSystem')
-    }
-
-    // Empty coords
-    // speciesCoords.map((coords: any, idx: number) => {
-    //   if (coords.lat === '' || coords.lon === '') {
-    //     console.log(idx)
-    //     job.log(`Empty coord: ${coords} (${SIGanalyst})`)
-    //     throw new Error('Empty latitude or longitude field.')
-    //   }
-    // })
-
-    // Remoção dos mesmos índices de speciesCoords, speciesUrns, speciesValidationOcc e speciesValidationSIG
-    if (flowData.flow === 'PNA' || flowData.flow === 'PA') {
-      const indicesToRemove: number[] = [];
-
-      speciesCoords = speciesCoords.filter(
-        (coords: { lat: string; lon: string }, index: number) => {
-          if (coords.lat === '' || coords.lon === '') {
-            indicesToRemove.push(index);
-            return false;
-          }
-          return true;
-        },
-      );
-
-      indicesToRemove.reverse().forEach((index) => {
-        speciesIds.splice(index, 1);
-        speciesValidationOcc.splice(index, 1);
-        speciesValidationSIG.splice(index, 1);
-      });
-    }
-
-    // Trim coords
-    speciesCoords = speciesCoords.map(
-      (coords: { lat: string; lon: string }) => ({
-        ...coords,
-        lat: coords.lat.toString().trim(),
-        lon: coords.lon.toString().trim(),
-      }),
-    );
-
-    // Check bad coordinates
-    job.updateProgress(2);
-
-    /// Coordinates format check
-    const regexValidLat = /^-?(90(\.0+)?|[0-8]?\d(\.\d+)?)$/;
-    const regexValidLon = /^-?(180(\.0+)?|(\d{1,2}|1[0-7]\d)(\.\d+)?)$/;
-
-    let haveInvalidCoordinateFormat: boolean = false;
-    let invalidCoordinateFormatCoords: any[] = [];
-
-    for (let i = 0; i < speciesCoords.length; i++) {
-      const isInvalidFormat =
-        !regexValidLat.test(speciesCoords[i].lat) ||
-        !regexValidLon.test(speciesCoords[i].lon);
-      if (isInvalidFormat) {
-        invalidCoordinateFormatCoords.push([
-          speciesCoords[i].lat,
-          speciesCoords[i].lon,
-        ]);
-        haveInvalidCoordinateFormat = true;
-      }
-    }
-
-    if (haveInvalidCoordinateFormat) {
-      invalidCoordinateFormatCoords.forEach((coords: any) => {
-        job.log(`Invalid coordinate format: ${coords} (${SIGanalyst})`);
-      });
-      throw new Error('Invalid coordinate format');
-    }
-
-    /// Check bounding box
-    const bbox = {
-      minLat: -57.632125,
-      maxLat: 23,
-      minLon: -102,
-      maxLon: -23.339269,
-    };
-
-    function isCoordinateInBoundingBox(lat: any, lon: any) {
-      return (
-        lat >= bbox.minLat &&
-        lat <= bbox.maxLat &&
-        lon >= bbox.minLon &&
-        lon <= bbox.maxLon
-      );
-    }
-
-    const coordsOutsideBoundingBox = speciesCoords
-      .map((coord: any) => {
-        const lat = parseFloat(coord.lat);
-        const lon = parseFloat(coord.lon);
-
-        if (!isCoordinateInBoundingBox(lat, lon)) {
-          return [coord.lat, coord.lon];
-        }
-      })
-      .filter(Boolean);
-
-    if (coordsOutsideBoundingBox.length > 0) {
-      coordsOutsideBoundingBox.forEach((coords: any) => {
-        job.log(`Coordinates outside bounding box: ${coords} (${SIGanalyst})`);
-      });
-      throw new Error('Coordinates outside bounding box');
-    }
-
-    /// Coordinates in water
-    let haveCoordsInWater: boolean = false;
-    let coordsInWater: any = [];
-    // https://hub.arcgis.com/datasets/CESJ::world-countries/explore?location=-3.705351%2C-31.396331%2C6.00
-    const worldCountriesPath = './src/queues/geojsons/World_Countries.json';
-    let worldCountries: any = readFileSync(worldCountriesPath, 'utf-8');
-    worldCountries = JSON.parse(worldCountries);
-
-    const areas = ['South America'];
-    let polygons = worldCountries.features.filter(
-      (feature: any) => areas.indexOf(feature.properties.CONTINENT) !== -1,
-    );
-    polygons = worldCountries.features.map((feature: any) =>
-      turf.multiPolygon(feature.geometry.coordinates),
-    );
-
-    coordsInWater = speciesCoords
-      .map((coord: any) => {
-        const point = turf.point([Number(coord.lon), Number(coord.lat)]);
-        let water: any = [];
-
-        const isInWater = polygons.some((poly: any) => {
-          const ptsWithin = turf.booleanPointInPolygon(point, poly);
-          if (ptsWithin) {
-            water.push(1);
-          } else {
-            water.push(0);
-          }
-
-          return ptsWithin;
-        });
-
-        if (
-          isInWater &&
-          water.reduce((total: any, num: any) => total + num, 0) === 0
-        ) {
-          return [coord.lat, coord.lon];
-        }
-        return null;
-      })
-      .filter(Boolean);
-
-    haveCoordsInWater = coordsInWater.length > 0;
-
-    if (haveCoordsInWater === true) {
-      coordsInWater.forEach((coords: any) => {
-        job.log(`Coordinates in water: ${coords} (${SIGanalyst})`);
-      });
-      throw new Error('Coordinates in water');
+    if (
+      source === 'Museu-Goeldi/PA' ||
+      source === 'CNCFlora-ProFlora' ||
+      source === 'CNCFlora-oldSystem'
+    ) {
+      flowData = await whichFlow(species, source);
+      SIGanalyst = await getSIGanalyst(species, source);
     }
 
     // Filter occurrences by flow
@@ -338,8 +184,8 @@ export class Processor_records extends WorkerHost {
           }
         }
       }
-
     }
+
 
     let speciesRecords: any = [];
     filteredOccIdx.forEach((i) => {
@@ -357,10 +203,135 @@ export class Processor_records extends WorkerHost {
     speciesRecords = geojson.parse(speciesRecords, { Point: ['lat', 'lon'] });
     speciesRecords = speciesRecords.features;
     speciesRecords = speciesRecords.filter((obj: any) =>
-      obj.geometry.hasOwnProperty('coordinates'),
+      obj.geometry.hasOwnProperty('coordinates')
     );
 
+    // Empty coords
+    const filteredSpeciesCoords: any[] = [];
+    speciesRecords.forEach((coord: any) => {
+      if (coord.geometry.coordinates[0] !== '' && coord.geometry.coordinates[1] !== '') {
+        filteredSpeciesCoords.push(coord.geometry.coordinates);
+      } else {
+        job.log(
+          `Empty coord: ${coord.geometry.coordinates[0]}, ${coord.geometry.coordinates[1]} (${SIGanalyst}) [id: ${coord.properties.id}]`
+        );
+      }
+    });
 
+    // Trim coords
+    filteredSpeciesCoords.forEach((coord: any) => {
+      if (typeof coord.lat === 'string') {
+        coord.lat = coord.lat.trim();
+      }
+      if (typeof coord.lon === 'string') {
+        coord.lon = coord.lon.trim();
+      }
+    });
+
+    // Check bad coordinates format
+    const regexValidLat = /^-?(90(\.0+)?|[0-8]?\d(\.\d+)?)$/;
+    const regexValidLon = /^-?(180(\.0+)?|(\d{1,2}|1[0-7]\d)(\.\d+)?)$/;
+
+    const invalidCoordinateFormatCoords: any[] = [];
+    speciesRecords.forEach((coord: any) => {
+      if (
+        !regexValidLat.test(coord.geometry.coordinates[0]) ||
+        !regexValidLon.test(coord.geometry.coordinates[1])
+      ) {
+        invalidCoordinateFormatCoords.push({
+          id: coord.properties.id,
+          lat: coord.geometry.coordinates[0],
+          lon: coord.geometry.coordinates[1]
+        });
+      }
+    });
+
+    if (invalidCoordinateFormatCoords.length > 0) {
+      invalidCoordinateFormatCoords.forEach((coord: any) => {
+        job.log(
+          `Invalid coordinate format: ${coord.lat}, ${coord.lon} (${SIGanalyst}) [id: ${coord.id}]`
+        );
+      });
+      throw new Error('Invalid coordinate format');
+    }
+
+    // Check bounding box
+    const bbox = {
+      minLat: -57.632125,
+      maxLat: 23,
+      minLon: -102,
+      maxLon: -23.339269,
+    };
+
+    const coordsOutsideBoundingBox: any[] = [];
+    speciesRecords.forEach((coord: any) => {
+      const lat = parseFloat(coord.geometry.coordinates[1]);
+      const lon = parseFloat(coord.geometry.coordinates[0]);
+      if (
+        lat < bbox.minLat ||
+        lat > bbox.maxLat ||
+        lon < bbox.minLon ||
+        lon > bbox.maxLon
+      ) {
+        coordsOutsideBoundingBox.push({
+          id: coord.properties.id,
+          coords: [coord.geometry.coordinates[0], coord.geometry.coordinates[1]]
+        });
+      }
+    });
+
+    if (coordsOutsideBoundingBox.length > 0) {
+      coordsOutsideBoundingBox.forEach((coord: any) => {
+        job.log(
+          `Coordinates outside bounding box: ${coord.coords} (${SIGanalyst}) [id: ${coord.id}]`
+        );
+      });
+      throw new Error('Coordinates outside bounding box');
+    }
+
+    // Coordinates in water
+    const worldCountriesPath = './src/queues/geojsons/World_Countries.json';
+    let worldCountries: any = readFileSync(worldCountriesPath, 'utf-8');
+    worldCountries = JSON.parse(worldCountries);
+
+    const areas = ['South America'];
+    let polygons = worldCountries.features
+      .filter((feature: any) => areas.indexOf(feature.properties.CONTINENT) !== -1)
+      .map((feature: any) => turf.multiPolygon(feature.geometry.coordinates));
+
+    const coordsInWater: any[] = [];
+    speciesRecords.forEach((coord: any) => {
+      const point = turf.point([Number(coord.geometry.coordinates[1]), Number(coord.geometry.coordinates[0])]);
+      let water: any = [];
+
+      const isInWater = polygons.some((poly: any) => {
+        const ptsWithin = turf.booleanPointInPolygon(point, poly);
+        if (ptsWithin) {
+          water.push(1);
+        } else {
+          water.push(0);
+        }
+
+        return ptsWithin;
+      });
+
+      if (isInWater && water.reduce((total: any, num: any) => total + num, 0) === 0) {
+        coordsInWater.push({
+          id: coord.properties.id,
+          coords: [coord.geometry.coordinates[0], coord.geometry.coordinates[1]]
+        });
+      }
+    });
+
+    if (coordsInWater.length > 0) {
+      coordsInWater.forEach((coords: any) => {
+        job.log(`Coordinates in water: ${coords.coords} (${SIGanalyst}) [id: ${coords.id}]`);
+      });
+      throw new Error('Coordinates in water');
+    }
+
+
+    // Write file
     writeFile(
       `G:/Outros computadores/Meu computador/CNCFlora_data/records/${species}.json`,
       JSON.stringify(speciesRecords),
@@ -369,7 +340,7 @@ export class Processor_records extends WorkerHost {
         if (err) {
           console.error(err);
         }
-      },
+      }
     );
 
     job.updateProgress(100);
